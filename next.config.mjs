5a364de74f5beef615d756f530fb82fe55b06807
arif6474 <arifulislam64743@gmail.com>
2026-04-25 02:46:21 +0600

    feat: integrate PWA support and update manifest icons

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,25 @@
+import withPWA from 'next-pwa';
+
+const withPWAConfig = withPWA({
+  dest: 'public',
+  disable: false,
+  register: true,
+  skipWaiting: true,
+  runtimeCaching: [
+    {
+      urlPattern: /^https:\/\/.*/i,
+      handler: 'NetworkFirst',
+      options: {
+        cacheName: 'offlineCache',
+        expiration: {
+          maxEntries: 200
+        }
+      }
+    }
+  ]
+});
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {};
 
-export default nextConfig;
+export default withPWAConfig(nextConfig);
