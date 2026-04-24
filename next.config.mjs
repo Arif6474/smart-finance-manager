f65c8efeca201ff7a03ffb07576ee5b34f311565
arif6474 <arifulislam64743@gmail.com>
2026-04-25 02:50:54 +0600

    Revert "feat: integrate PWA support and update manifest icons"
    This reverts commit 5a364de74f5beef615d756f530fb82fe55b06807.

--- next.config.mjs
+++ next.config.mjs
@@ -1,25 +1,4 @@
-import withPWA from 'next-pwa';
-
-const withPWAConfig = withPWA({
-  dest: 'public',
-  disable: false,
-  register: true,
-  skipWaiting: true,
-  runtimeCaching: [
-    {
-      urlPattern: /^https:\/\/.*/i,
-      handler: 'NetworkFirst',
-      options: {
-        cacheName: 'offlineCache',
-        expiration: {
-          maxEntries: 200
-        }
-      }
-    }
-  ]
-});
-
 /** @type {import('next').NextConfig} */
 const nextConfig = {};
 
-export default withPWAConfig(nextConfig);
+export default nextConfig;
